818bbce781daac44c17d238208a916dc7e32992a
Lucas de Mira <lucasmirao@hotmail.com>
2025-05-20 16:03:23 -0300

    chore: add CORS support to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import router from './routes/index.js';
+import cors from 'cors';
 import './config/dbConfig.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
